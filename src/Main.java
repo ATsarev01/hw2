c3af06da65bbf63808cb06d2e294948de94055c2
ATsarev01 <maxperov01@gmail.com>
2023-02-13 22:08:09 +0600

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        var dog = 8.0;
+    System.out.println(dog);
+        dog = dog + 4;
+        System.out.println(dog);
+        dog = dog - 3.5;
+        System.out.println(dog);
+    var cat = 3.6;
+    System.out.println(cat);
+    cat = + cat + 4;
+        System.out.println(cat);
+        cat = + cat - 1.6;
+        System.out.println(cat);
+    var paper = 763789;
+    System.out.println(paper);
+    paper = paper + 4;
+        System.out.println(paper);
+        paper = paper - 7639;
+        System.out.println(paper);
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+var weight1 = 78.2;
+var weight2 = 82.7;
+var mainWeight = weight1 + weight2;
+        System.out.println("Общий вес боксеров " +  mainWeight  +  " кг!");
+        var differenseWeight = weight2 - weight1;
+        System.out.println("Разница в весе " + differenseWeight + " кг!");
+        var differenseWeight2 = weight2 % weight1;
+        System.out.println("Разница в весе " + differenseWeight2 + " кг!");
+        var workingHours = 640;
+        var workersHours = 8;
+        var workers = workingHours / workersHours;
+        System.out.println("Всего работников в компании - " + workers + " человек!");
+        var allWorkers = workers + 94;
+        var allHours = allWorkers * 8;
+        System.out.println("Если в компании работает " + allWorkers + " человек, то всего " + allHours + " часов работы может быть поделено между сотрудниками");
+
     }
 }
